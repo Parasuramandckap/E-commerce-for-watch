65219fd5d9ccda8821f3135ac80fa13d4de97d7a
Parasuramandckap <parasuaptadckap@gmail.com>
2023-04-28 23:46:47 +0530

    search functionality

--- home-page/lending.js
+++ home-page/lending.js
@@ -5,54 +5,13 @@ let mostLovedproduct = document.querySelectorAll(".product-list")
 window.addEventListener("DOMContentLoaded",()=>{
     fetch("http://localhost:3000/products/")
     .then(res=>res.json())
-    .then(data=>{
+    .then(productsCreate=>{
         // console.log(data)
-        for (let a = 0; a < data.length; a++) {
-            //create div for product details
-            let  product_details = document.createElement("div")
-            product_details.setAttribute("class","product-detatils")
-            
-            //create div for heartdiv icon
-            let heartDiv =  document.createElement("div")
-            heartDiv.setAttribute("class","heart")
-            heartDiv.innerHTML = `<i class="fa-regular fa-heart"></i>`
-            product_details.appendChild(heartDiv)
-            
-            //product imges
-            let product_img = document.createElement("img")
-            product_img.setAttribute("class","product-img")
-            product_img.setAttribute("src",data[a].images)
-            product_details.appendChild(product_img)
-            
-            // element for brand 
-            let brand = document.createElement("p")
-            brand.innerHTML = `<a>${data[a].brand}</a>`
-            brand.setAttribute("class","brand")
-            product_details.appendChild(brand)
-            
-            // element for model_name 
-            let model_name = document.createElement("p")
-            model_name.innerText = `${data[a].series}`
-            model_name.setAttribute("class","model-name")
-            product_details.appendChild(model_name)
-            
-            // element for price
-            let price_rate = document.createElement("h6")
-            price_rate.innerHTML = `${data[a].price}<span class="discount">${data[a].discount}</span><span class="offer">${data[a].offer}</span>`
-            price_rate.setAttribute("class","price-rate")
-            product_details.appendChild(price_rate)
-            
-            // element for trend
-            let trend = document.createElement("p")
-            trend.setAttribute("class","trend")
-            trend.innerHTML = `<a class="trending">Treanding</a>`
-            product_details.appendChild(trend)
-            
-            //append product div to products
-            products.appendChild(product_details)
-        }
+        productsCreate.forEach((productsCreate)=>{
+            productGenarate(productsCreate)
+        })
         for(let i=0;i<4;i++){
-            console.log(data[i])
+            // console.log(productsCreate[i])
             let mostDiv = document.createElement("div")
             mostDiv.setAttribute("class","loved-list")
             
@@ -68,17 +27,17 @@ window.addEventListener("DOMContentLoaded",()=>{
             
             
             let most_love_img = document.createElement("img")
-            most_love_img.setAttribute("src",data[i].images)
+            most_love_img.setAttribute("src",productsCreate[i].images)
             product_list.appendChild(most_love_img)
             
             let brand =  document.createElement("p")
             brand.setAttribute("class","brand")
-            brand.innerHTML = `<a>${data[i].brand}</a>`
+            brand.innerHTML = `<a>${productsCreate[i].brand}</a>`
             product_list.appendChild(brand)
-            
+ 
             let price = document.createElement("p")
             price.setAttribute("class","price-rate")
-            price.innerHTML = `${data[i].price}<span class="discount">${data[i].discount}</span><span class="offer">${data[i].offer}</span>`
+            price.innerHTML = `${productsCreate[i].price}<span class="discount">${productsCreate[i].discount}</span><span class="offer">${productsCreate[i].offer}</span>`
             product_list.appendChild(price)
             
             highlight.appendChild(mostDiv)
@@ -86,8 +45,53 @@ window.addEventListener("DOMContentLoaded",()=>{
     }).catch((error)=>console.log(error))
 })
 
-//addEventlisterner for filter
+function productGenarate(productsCreate){
+    //create div for product details
+    let  product_details = document.createElement("div")
+    product_details.setAttribute("class",`product-detatils ${productsCreate.gender}`)
+    
+    //create div for heartdiv icon
+    let heartDiv =  document.createElement("div")
+    heartDiv.setAttribute("class","heart")
+    heartDiv.innerHTML = `<i class="fa-regular fa-heart"></i>`
+    product_details.appendChild(heartDiv)
+    
+    //product imges
+    let product_img = document.createElement("img")
+    product_img.setAttribute("class","product-img")
+    product_img.setAttribute("src",productsCreate.images)
+    product_details.appendChild(product_img)
+    
+    // element for brand 
+    let brand = document.createElement("p")
+    brand.innerHTML = `<a>${productsCreate.brand}</a>`
+    brand.setAttribute("class","brand")
+    product_details.appendChild(brand)
+    //call the brand to filter products function for filter
+    filler_products(brand)
+    // element for model_name 
+    let model_name = document.createElement("p")
+    model_name.innerText = `${productsCreate.series}`
+    model_name.setAttribute("class","model-name")
+    product_details.appendChild(model_name)
+    
+    // element for price
+    let price_rate = document.createElement("h6")
+    price_rate.innerHTML = `${productsCreate.price}<span class="discount">${productsCreate.discount}</span><span class="offer">${productsCreate.offer}</span>`
+    price_rate.setAttribute("class","price-rate")
+    product_details.appendChild(price_rate)
+    
+    // element for trend
+    let trend = document.createElement("p")
+    trend.setAttribute("class","trend")
+    trend.innerHTML = `<a class="trending">Treanding</a>`
+    product_details.appendChild(trend)
+    
+    //append product div to products
+    products.appendChild(product_details)
+}
 
+//addEventlisterner for filter
 let filterIcon = document.querySelector(".filler-icon")
 let filterDiv = document.querySelector(".filter")
 
@@ -97,4 +101,56 @@ filterIcon.addEventListener("click",()=>{
     // hide(document.querySelector(".wishlist"))
     // hide(document.querySelector(".cart"))
 })
+
+
+
+let search = document.querySelector(".search")
+
+function filler_products(brandName){
+    // console.log(name)
+    search.addEventListener("keyup",(e)=>{
+        let searchWord = e.target.value.toLowerCase()
+        if(brandName.innerText.toLowerCase()===searchWord){
+            brandName.parentElement.style.display = "block"
+        }else{
+            brandName.parentElement.style.display = "none"
+        }
+    })
+
+}
+//this function for angerTag click add color
+let angerTag = document.querySelectorAll(".category a")
+angerTag.forEach(element => {
+    element.addEventListener("click",(e)=>{
+        angerTag.forEach((aTag)=>{
+            if(e.target.innerText.toLowerCase()== aTag.innerText.toLowerCase()){
+                aTag.style.color = "#10CED8"
+            }
+            else{
+                aTag.style.color = "#182C3E"
+            }
+        })
+    })
+});
+
+
+//filter
+let menAll = document.querySelector("#menAll")
+let product =  document.querySelector(".products")
+console.log(product)
+menAll.addEventListener("click",(e)=>{
+    if(menAll.checked){
+        let allproduct = document.querySelectorAll(".product-detatils")
+        allproduct.forEach((element)=>{
+            if(element.classList.contains("male")){
+                console.log(element)
+            }
+        })
+    }else{
+        console.log("not ok")
+    }
+})
+
+
+
 
