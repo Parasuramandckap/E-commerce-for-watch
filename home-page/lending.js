eeb4afb6c4723f8d199a11056aec755cacd9076c
Parasuramandckap <parasuaptadckap@gmail.com>
2023-04-28 17:33:52 +0530

    elements create in js

--- home-page/lending.js
+++ home-page/lending.js
@@ -1,5 +1,6 @@
 let Allproducts = document.querySelectorAll(".product-detatils")
-
+let products = document.querySelector(".products")
+let highlight = document.querySelector(".highlight")
 let mostLovedproduct = document.querySelectorAll(".product-list")
 window.addEventListener("DOMContentLoaded",()=>{
     fetch("http://localhost:3000/products/")
@@ -7,21 +8,83 @@ window.addEventListener("DOMContentLoaded",()=>{
     .then(data=>{
         // console.log(data)
         for (let a = 0; a < data.length; a++) {
-            for(let a=0;a<Allproducts.length;a++){
-                Allproducts[a].children[1].src = data[a].images
-                Allproducts[a].children[2].innerText = data[a].brand
-                Allproducts[a].children[3].innerText= data[a].series
-                Allproducts[a].children[4].innerHTML= `${data[a].price}<span class="discount">${data[a].discount}</span><span class="offer">${data[a].offer}</span>`
-            }
-            for(let p=0;p<mostLovedproduct.length;p++){
-                mostLovedproduct[p].children[0].src = data[p].images;
-                mostLovedproduct[p].children[1].children[0].innerText =data[p].brand
-                mostLovedproduct[p].children[2].innerHTML =`${data[a].price}<span class="discount">${data[a].discount}</span><span class="offer">${data[a].offer}</span>`
-            }
+            //create div for product details
+            let  product_details = document.createElement("div")
+            product_details.setAttribute("class","product-detatils")
+            
+            //create div for heartdiv icon
+            let heartDiv =  document.createElement("div")
+            heartDiv.setAttribute("class","heart")
+            heartDiv.innerHTML = `<i class="fa-regular fa-heart"></i>`
+            product_details.appendChild(heartDiv)
+            
+            //product imges
+            let product_img = document.createElement("img")
+            product_img.setAttribute("class","product-img")
+            product_img.setAttribute("src",data[a].images)
+            product_details.appendChild(product_img)
+            
+            // element for brand 
+            let brand = document.createElement("p")
+            brand.innerHTML = `<a>${data[a].brand}</a>`
+            brand.setAttribute("class","brand")
+            product_details.appendChild(brand)
+            
+            // element for model_name 
+            let model_name = document.createElement("p")
+            model_name.innerText = `${data[a].series}`
+            model_name.setAttribute("class","model-name")
+            product_details.appendChild(model_name)
+            
+            // element for price
+            let price_rate = document.createElement("h6")
+            price_rate.innerHTML = `${data[a].price}<span class="discount">${data[a].discount}</span><span class="offer">${data[a].offer}</span>`
+            price_rate.setAttribute("class","price-rate")
+            product_details.appendChild(price_rate)
+            
+            // element for trend
+            let trend = document.createElement("p")
+            trend.setAttribute("class","trend")
+            trend.innerHTML = `<a class="trending">Treanding</a>`
+            product_details.appendChild(trend)
+            
+            //append product div to products
+            products.appendChild(product_details)
+        }
+        for(let i=0;i<4;i++){
+            console.log(data[i])
+            let mostDiv = document.createElement("div")
+            mostDiv.setAttribute("class","loved-list")
+            
+            let heartDiv = document.createElement("div")
+            heartDiv.setAttribute("class","heart")
+            heartDiv.innerHTML = `<i class="fa-regular fa-heart"></i>`
+            
+            mostDiv.appendChild(heartDiv)
+            
+            let product_list = document.createElement("div")
+            product_list.setAttribute("class","product-list")
+            mostDiv.appendChild(product_list)
+            
+            
+            let most_love_img = document.createElement("img")
+            most_love_img.setAttribute("src",data[i].images)
+            product_list.appendChild(most_love_img)
+            
+            let brand =  document.createElement("p")
+            brand.setAttribute("class","brand")
+            brand.innerHTML = `<a>${data[i].brand}</a>`
+            product_list.appendChild(brand)
+            
+            let price = document.createElement("p")
+            price.setAttribute("class","price-rate")
+            price.innerHTML = `${data[i].price}<span class="discount">${data[i].discount}</span><span class="offer">${data[i].offer}</span>`
+            product_list.appendChild(price)
+            
+            highlight.appendChild(mostDiv)
         }
     }).catch((error)=>console.log(error))
 })
-
 
 //addEventlisterner for filter
 
